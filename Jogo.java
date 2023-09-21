789ba7b62ce0a28a0ed14c065435b10363f07812
Vitoria <vitoriahellen3004@gmail.com>
2023-09-21 17:30:05 -0300

    ap07(poo-p2): caçador aleatorio

--- Jogo.java
+++ Jogo.java
@@ -1,8 +1,30 @@
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+
+import javax.tools.ForwardingJavaFileManager;
 public class Jogo {
     public static void main(String[] args) throws Exception {
+        var gerador = new Random(); 
         var personagem = new Personagem(); 
         personagem.nome = "John "; 
+        var soneca = new Personagem(); 
+        //nasce com muito sono e sem energia, fome mais ou menos 
+        //ele tem probabilidade 80% de dormir 
+        //10% de caçar 
+        //10% de comer
         while(true){
+            int oQuefazer = gerador.nextInt(3); 
+            switch(oQuefazer){
+                case 0: 
+                    personagem.cacar();
+                    break; 
+                case 1: 
+                    personagem.comer();
+                    break; 
+                case 2: 
+                    personagem.dormir(); 
+                    break; 
+            }
             personagem.cacar();
             personagem.comer();
             personagem.dormir();
